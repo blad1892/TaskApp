0488f40e7e43e9d693a02a2be86e7d7652c5406b
blad1892 <bladimirrv189@hotmail.com>
2018-06-11 23:34:00 -0400

    Inicializando repositorio

---
+++ app/src/main/java/ado/edu/itlas/taskapp/MainActivity.java
@@ -0,0 +1,18 @@
+package ado.edu.itlas.taskapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+    /*
+    *https://github.com/blad1892/TaskApp
+    *
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
