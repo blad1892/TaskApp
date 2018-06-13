b6f37a6e278fa5cfffa0a14a74ae6a61c3f93336
blad1892 <bladimirrv189@hotmail.com>
2018-06-13 14:19:51 -0400

    declarando variables en la clase categoria

--- app/src/main/java/ado/edu/itlas/taskapp/entidad/Categoria.java
+++ app/src/main/java/ado/edu/itlas/taskapp/entidad/Categoria.java
@@ -1,4 +1,27 @@
 package ado.edu.itlas.taskapp.entidad;
 
 public class Categoria {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String descripcion;
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", descripcion='" + descripcion + '\'' + '}';
+    }
 }
