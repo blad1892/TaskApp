aeb7eb74bfe2b1c68de22e3de958fc2e54ad7adb
blad1892 <bladimirrv189@hotmail.com>
2018-07-09 15:15:50 -0400

    Agregando la extructura de las tablas Tareas y Usuario

--- app/src/main/java/ado/edu/itlas/taskapp/repositorio/db/ExtructuraDb.java
+++ app/src/main/java/ado/edu/itlas/taskapp/repositorio/db/ExtructuraDb.java
@@ -4,5 +4,8 @@ package ado.edu.itlas.taskapp.repositorio.db;
 
 public class ExtructuraDb {
     public static final  String TABLA_CATEGORIA = "CREATE TABLE categoria(id INTEGER NOT NULL  PRIMARY KEY AUTOINCREMENT, nombre TEXT)";
-
+    public static final String TABLA_TAREAAS = "CREATE TABLE tareas(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, fecha DATE,"
+            + " fechaTerminado DATE, estado TEXT, categoria TEXT, usuarioCreador TEXT, usuarioAsignado TEXT)";
+    public  static final String TABAL_USUARIO="CREATE TABLE usuarios(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre TEXT, email TEXT, contraceña TEXT, "
+            +" tipoUsuario TEXT)";
 }
