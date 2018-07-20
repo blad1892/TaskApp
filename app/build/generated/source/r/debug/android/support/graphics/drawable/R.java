44d47189ce1a2295f250303bb9fc9b338b543fd3
blad1892 <bladimirrv189@hotmail.com>
2018-07-20 07:41:09 -0400

    Dando diseno de Activity Crear Tareas y Mostrar tarea creada

--- app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
@@ -85,22 +85,22 @@ public final class R {
         public static final int notification_main_column_container = 0x7f080085;
         public static final int right_icon = 0x7f080093;
         public static final int right_side = 0x7f080094;
-        public static final int tag_transition_group = 0x7f0800be;
-        public static final int text = 0x7f0800bf;
-        public static final int text2 = 0x7f0800c0;
-        public static final int time = 0x7f0800cf;
-        public static final int title = 0x7f0800d0;
+        public static final int tag_transition_group = 0x7f0800c0;
+        public static final int text = 0x7f0800c1;
+        public static final int text2 = 0x7f0800c2;
+        public static final int time = 0x7f0800d4;
+        public static final int title = 0x7f0800d5;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a0034;
-        public static final int notification_action_tombstone = 0x7f0a0035;
-        public static final int notification_template_custom_big = 0x7f0a003c;
-        public static final int notification_template_icon_group = 0x7f0a003d;
-        public static final int notification_template_part_chronometer = 0x7f0a0041;
-        public static final int notification_template_part_time = 0x7f0a0042;
+        public static final int notification_action = 0x7f0a0035;
+        public static final int notification_action_tombstone = 0x7f0a0036;
+        public static final int notification_template_custom_big = 0x7f0a003d;
+        public static final int notification_template_icon_group = 0x7f0a003e;
+        public static final int notification_template_part_chronometer = 0x7f0a0042;
+        public static final int notification_template_part_time = 0x7f0a0043;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0c003b;
---
+++ app/src/main/java/ado/edu/itlas/taskapp/vista/MostrarTareaCreada.java
@@ -0,0 +1,15 @@
+package ado.edu.itlas.taskapp.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ado.edu.itlas.taskapp.R;
+
+public class MostrarTareaCreada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_tarea_creada);
+    }
+}
