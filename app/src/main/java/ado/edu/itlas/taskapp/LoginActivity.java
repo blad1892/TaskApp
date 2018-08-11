33b50aa040c5bf56fab5e27972edc597405f8768
MESCyT <blad1892>
2018-08-11 11:00:46 -0400

    practica final

--- app/src/main/java/ado/edu/itlas/taskapp/LoginActivity.java
+++ app/src/main/java/ado/edu/itlas/taskapp/LoginActivity.java
@@ -1,14 +1,25 @@
 package ado.edu.itlas.taskapp;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import ado.edu.itlas.taskapp.entidad.Usuario;
 import ado.edu.itlas.taskapp.repositorio.UsuarioRepositorio;
@@ -29,6 +40,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+// ...
+
+// Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://www.google.com";
 
         // ////////////////////////////// implemetar gjon //////////////// //////
         //         Usuario usuario = nuevo Usuario ();
@@ -70,6 +89,7 @@ public class LoginActivity extends AppCompatActivity {
 //         }
         // ////////////////////////////////// Gson API //////////// /////////////////////////////
 
+
         usuarioRepositorio = new UsuarioRepositorioImp(this);
 
         Button btnIniciar = (Button) findViewById(R.id.btnIniciar);
@@ -80,11 +100,44 @@ public class LoginActivity extends AppCompatActivity {
         btnIniciar.setOnClickListener(new View.OnClickListener() {
                                           @Override
                                           public void onClick(View v) {
+
+
                                               usuario = new Usuario();
                                               EditText txtUsuario = (EditText) findViewById(R.id.txtNombre);
                                               EditText txtContracena = (EditText) findViewById(R.id.txtContraceña);
                                               usuario.setNombre(txtUsuario.getText().toString());
-                                              usuario = usuarioRepositorio.buscarUser(usuario.toString());
+
+                                              // Request a string response from the provided URL.
+                                              final Gson gson = new Gson();
+                                              final StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                                                      new Response.Listener<String>() {
+                                                          @Override
+                                                          public void onResponse(String response) {
+                                                              // Display the first 500 characters of the response string.
+//                                                              mTextView.setText("Response is: "+ response.substring(0,500));
+
+                                                              Log.i("RESUMEN", response);
+
+                                                              Usuario us = gson.fromJson(response, Usuario.class);
+                                                          }
+                                                      }, new Response.ErrorListener() {
+                                                  @Override
+                                                  public void onErrorResponse(VolleyError error) {
+//                                                      mTextView.setText("That didn't work!");
+                                                      Log.i("RESUMEN", error.toString());
+                                                  }
+                                              }) {
+                                                  @Override
+                                                  public byte[] getBody() throws AuthFailureError {
+
+                                                      String stringson = gson.toJson(usuario);
+                                                      return stringson.getBytes();
+                                                  }
+                                              };
+                                              queue.add(stringRequest);
+
+                                              //////////////////////////////////////////
+//                                              usuario = usuarioRepositorio.buscarUser(usuario.toString());
 
                                               if (txtNombre.getText().toString().equals("admin") && txtContracena.getText().toString().equals("admin")) {
                                                   Intent intento = new Intent(LoginActivity.this, MainActivity.class);
